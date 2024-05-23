02ff8f76aa7babde574e23903c7d4d77380e0607
Harsh sahu <sahuharsh164@gmail.com>
2024-05-23 15:55:51 +0530

    suspense loading

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import Banner from "@/app/(home)/components/Banner";
 import Image from "next/image";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 export default async function Home() {
   //data fetching
@@ -8,7 +10,9 @@ export default async function Home() {
   return (
     <>
       <Banner />
+      <Suspense fallback={<Loading />}>
       <BookList />
+      </Suspense>
     </>
   );
 }
